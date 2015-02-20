74b108693ce631075e70463814a185b1d9d3d585
sistazolf <natayakitti@gmail.com>
2015-02-20 13:21:25 +0700

    edit- add on

---
+++ app/src/main/java/th/ac/tu/siit/its333/lab4exercise1/MainActivity.java
@@ -0,0 +1,158 @@
+package th.ac.tu.siit.its333.lab4exercise1;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    CourseDBHelper helper;
+    SimpleCursorAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        helper = new CourseDBHelper(this.getApplicationContext());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // This method is called when this activity is put foreground.
+        SQLiteDatabase db = helper.getReadableDatabase();
+        Cursor cursor = db.rawQuery(
+                "SELECT SUM(value*credit) gp, SUM(credit) cr FROM course;", null);
+        cursor.moveToFirst();
+        double gp = cursor.getDouble(0);
+        int cr = cursor.getInt(1);
+        double gpa = 0.0;
+
+        if (cr != 0) {
+            gpa = gp/cr;
+        }
+
+        TextView tvGP = (TextView)findViewById(R.id.tvGP);
+        TextView tvCR = (TextView)findViewById(R.id.tvCR);
+        TextView tvGPA = (TextView)findViewById(R.id.tvGPA);
+
+        tvGP.setText(String.format("%.1f", gp));
+        tvCR.setText(String.format("%d", cr));
+        tvGPA.setText(String.format("%.2f", gpa));
+
+        db.close();
+    }
+
+    public void buttonClicked(View v) {
+        int id = v.getId();
+        Intent i;
+
+        switch(id) {
+            case R.id.btAdd:
+                i = new Intent(this, AddCourseActivity.class);
+                startActivityForResult(i, 88);
+                break;
+
+            case R.id.btShow:
+                i = new Intent(this, ListCourseActivity.class);
+                startActivity(i);
+                break;
+
+            case R.id.btReset:
+                SQLiteDatabase db = helper.getWritableDatabase();
+                db.delete("course", "", null);
+                this.onResume();
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 88) {
+            if (resultCode == RESULT_OK) {
+                String code = data.getStringExtra("code");
+                int credit = data.getIntExtra("credit", 0);
+                String grade = data.getStringExtra("grade");
+
+                SQLiteDatabase db = helper.getWritableDatabase();
+                ContentValues r = new ContentValues();
+                r.put("code", code);
+                r.put("credit", credit);
+                r.put("grade", grade);
+                r.put("value", gradeToValue(grade));
+                long newId = db.insert("course", null, r);
+
+                if (newId != -1) {
+                    Toast t = Toast.makeText(this.getApplicationContext(),
+                            "Successfully added a new grade",
+                            Toast.LENGTH_SHORT);
+                    t.show();
+                }
+                else {
+                    Toast t = Toast.makeText(this.getApplicationContext(),
+                            "Unable to add a new grade",
+                            Toast.LENGTH_SHORT);
+                    t.show();
+                }
+            }
+        }
+
+        Log.d("course", "onActivityResult");
+    }
+
+    static double gradeToValue(String g) {
+        if (g.equals("A"))
+            return 4.0;
+        else if (g.equals("B+"))
+            return 3.5;
+        else if (g.equals("B"))
+            return 3.0;
+        else if (g.equals("C+"))
+            return 2.5;
+        else if (g.equals("C"))
+            return 2.0;
+        else if (g.equals("D+"))
+            return 1.5;
+        else if (g.equals("D"))
+            return 1.0;
+        else
+            return 0.0;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
